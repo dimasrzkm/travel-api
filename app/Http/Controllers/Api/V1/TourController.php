d206368106a4cf42f281e2c7cffeb60de8e5d35d
Dimas Rizki Mahendra <90965333+dimasrzkm@users.noreply.github.com>
2023-11-20 07:30:52 +0700

    Tours filtering and ordering

--- app/Http/Controllers/Api/V1/TourController.php
+++ app/Http/Controllers/Api/V1/TourController.php
@@ -3,14 +3,30 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\TourListRequest;
 use App\Http\Resources\TourResource;
 use App\Models\Travel;
 
 class TourController extends Controller
 {
-    public function index(Travel $travel)
+    public function index(TourListRequest $request, Travel $travel)
     {
         $tours = $travel->tours()
+            ->when($request->priceFrom, function ($query) use ($request) {
+                return $query->where('price', '>=', $request->priceFrom * 100);
+            })
+            ->when($request->priceTo, function ($query) use ($request) {
+                return $query->where('price', '<=', $request->priceTo * 100);
+            })
+            ->when($request->dateFrom, function ($query) use ($request) {
+                return $query->where('starting_date', '>=', $request->dateFrom);
+            })
+            ->when($request->dateTo, function ($query) use ($request) {
+                return $query->where('starting_date', '<=', $request->dateTo);
+            })
+            ->when($request->sortBy && $request->sortOrder, function ($query) use ($request) {
+                return $query->orderBy($request->sortBy, $request->sortOrder);
+            })
             ->orderBy('starting_date')
             ->paginate();
 
